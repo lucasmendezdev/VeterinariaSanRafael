47ff2c1bdafa1b35edf92d772f78436541dba459
lucasmendezdev <lucasmendezdev@gmail.com>
2026-02-22 11:51:40 -0300

    SUBO EL PROYECTO

---
+++ js/data.js
@@ -0,0 +1,83 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "DR COSSIA CACHORRO",
+    precio: 12000,
+    categoria: "perro",
+    tipo: "alimentoPerro",
+    imagen: "assets/img/drCossiaCachorro.png",
+    talles: ["M", "L", "XL"],
+    descripcion: "Remera de algodón, cómoda y versátil para todos los días."
+  },
+  {
+    id: 2,
+    nombre: "DR COSSIA ADULTO",
+    precio: 22000,
+    categoria: "perro",
+    tipo: "alimentoPerro",
+    imagen: "assets/img/drCossiaAdulto.png",
+    talles: ["M", "L", "XL"],
+    descripcion: "Buzo oversize de frisa, ideal para un look urbano."
+  },
+  {
+    id: 3,
+    nombre: "DR COSSIA CLASSIC CAT",
+    precio: 22000,
+    categoria: "gato",
+    tipo: "alimentoGato",
+    imagen: "assets/img/drCossiaClassicCat.png",
+    talles: ["M", "L", "XL"],
+    descripcion: "Buzo oversize de frisa, ideal para un look urbano."
+  },
+  {
+    id: 4,
+    nombre: "DR COSSIA SUPER PREMIUM CAT",
+    precio: 22000,
+    categoria: "gato",
+    tipo: "alimentoGato",
+    imagen: "assets/img/drCossiaSuperPremiumCat.png",
+    talles: ["M", "L", "XL"],
+    descripcion: "Buzo oversize de frisa, ideal para un look urbano."
+  }
+  ,
+  {
+    id: 5,
+    nombre: "AP ADULTO",
+    precio: 22000,
+    categoria: "perro",
+    tipo: "alimentoPerro",
+    imagen: "assets/img/apAdulto.jpg",
+    talles: ["M", "L", "XL"],
+    descripcion: "Buzo oversize de frisa, ideal para un look urbano."
+  },
+  {
+    id: 6,
+    nombre: "SABROSITOS GATO",
+    precio: 22000,
+    categoria: "gato",
+    tipo: "alimentoGato",
+    imagen: "assets/img/sabrositosGato.png",
+    talles: ["M", "L", "XL"],
+    descripcion: "Buzo oversize de frisa, ideal para un look urbano."
+  },
+  {
+    id: 7,
+    nombre: "SABROSITOS ADULTO",
+    precio: 22000,
+    categoria: "perro",
+    tipo: "alimentoPerro",
+    imagen: "assets/img/sabrositosAdulto.png",
+    talles: ["M", "L", "XL"],
+    descripcion: "Buzo oversize de frisa, ideal para un look urbano."
+  },
+  {
+    id: 8,
+    nombre: "SABROSITOS CACHORRO",
+    precio: 22000,
+    categoria: "perro",
+    tipo: "alimentoPerro",
+    imagen: "assets/img/sabrositosC",
+    talles: ["M", "L", "XL"],
+    descripcion: "Buzo oversize de frisa, ideal para un look urbano."
+  }
+];
