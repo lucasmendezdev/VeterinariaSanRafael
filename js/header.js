8ebc550ce279f955d91878a2af681a9ff0aa9959
lucasmendezdev <lucasmendezdev@gmail.com>
2026-02-23 23:02:25 -0300

    RETOMAMOS

--- js/header.js
+++ js/header.js
@@ -11,3 +11,11 @@ function actualizarContadorCarrito() {
 document.addEventListener("DOMContentLoaded", () => {
   actualizarContadorCarrito();
 });
+
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
